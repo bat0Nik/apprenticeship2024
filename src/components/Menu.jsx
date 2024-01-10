b106a2ad3cf689bad46be40f523645c5cd751681
Kamil <kamil.boho27@gmail.com>
2024-01-10 10:43:24 +0100

    soundbar

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 import logo from "../images/LOGOMathGAMEv2.png";
-// import logo from "../images/logoMathGAME.png";
+import Soundbar from "./Soundbar";
 
 const Menu = () => {
   return (
     <div className="menu-container">
       <img src={logo} alt="logo" />
       <Link to="/modes">GRAJ</Link>
+      <Soundbar />
     </div>
   );
 };
