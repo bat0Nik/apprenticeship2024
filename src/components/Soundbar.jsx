f1fa3cc8254a0621a0fcfdf784cbb0fb2c2675e4
Kamil <kamil.boho27@gmail.com>
2024-01-10 12:55:06 +0100

    czystykod

--- src/components/Soundbar.jsx
+++ src/components/Soundbar.jsx
@@ -4,7 +4,7 @@ import { useAudio } from './AudioContext.jsx';
 import Gra from '../sound/gra.mp3';
 
 const Soundbar = () => {
-  const { play,volume, volumeChange } = useAudio();
+const { play,volume, volumeChange } = useAudio();
 
 useEffect(() => {
     play(Gra);
@@ -12,20 +12,20 @@ useEffect(() => {
 const volumeHandler = (e) => {
     const newVolume = parseFloat(e.target.value);
     volumeChange(newVolume);
-  };
+};
 
-  return (
+return (
     <div>
-      <input
+    <input
         type="range"
         min={0}
         max={1}
         step={0.01}
         value={volume}
         onChange={volumeHandler}
-      />
+    />
     </div>
-  );
+);
 };
 
 export default Soundbar;
